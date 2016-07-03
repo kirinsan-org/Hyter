edd2921d43836d2196d901de1b4958370d2bebd8
星野恵瑠 <shonetesmith@gmail.com>
2016-07-03 16:19:54 +0900

    Initial Commit

---
+++ Hyter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hyter
+//
+//  Created by 史翔新 on 2016/07/03.
+//  Copyright © 2016年 Crazism. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
