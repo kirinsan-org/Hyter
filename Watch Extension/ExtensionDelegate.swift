ed792a0fb4c8e080b83e3df528c1fec86b86001e
星野恵瑠 <shonetesmith@gmail.com>
2016-09-11 04:33:27 +0900

    Project settings update

--- Watch Extension/ExtensionDelegate.swift
+++ Watch Extension/ExtensionDelegate.swift
@@ -23,7 +23,8 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
         // Use this method to pause ongoing tasks, disable timers, etc.
     }
 
-    func handle(_ backgroundTasks: Set<WKRefreshBackgroundTask>) {
+	@available(watchOSApplicationExtension 3.0, *)
+	func handle(_ backgroundTasks: Set<WKRefreshBackgroundTask>) {
         // Sent when the system needs to launch the application in the background to process tasks. Tasks arrive in a set, so loop through and process each one.
         for task in backgroundTasks {
             // Use a switch statement to check the task type
